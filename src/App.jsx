064d1945d127512e106480ef14d17c61711bcbd0
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-23 17:33:01 +0200

    add decimal function

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,47 @@ function App() {
   const [store, setStore] = useState('0')
 
   function handleClickFirstNumber(e) {
-    if (firstNumber > 0 ) {
-      setFirstNumber(firstNumber + e.target.innerText)
+    if (!firstNumber.includes('.')) {
+      if (firstNumber > 0 ) {
+        setFirstNumber(firstNumber + e.target.innerText)
+      } else {
+        setFirstNumber(e.target.innerText)
+      }
+    } else if (firstNumber.includes('.') && e.target.innerText === '.') {
+      if (firstNumber > 0 ) {
+        setFirstNumber(firstNumber + '')
+      } else {
+        setFirstNumber(e.target.innerText)
+      }
     } else {
-      setFirstNumber(e.target.innerText)
+      if (firstNumber > 0 || firstNumber === '.') {
+        setFirstNumber(firstNumber + e.target.innerText)
+      } else {
+        setFirstNumber(e.target.innerText)
+      }
     }
+    
   }
 
   function handleClickSecondNumber(e) {
-    if (secondNumber > 0 ) {
-      setSecondNumber(secondNumber + e.target.innerText)
+    if (!secondNumber.includes('.')) {
+      if (secondNumber > 0 ) {
+        setSecondNumber(secondNumber + e.target.innerText)
+      } else {
+        setSecondNumber(e.target.innerText)
+      }
+    } else if (secondNumber.includes('.') && e.target.innerText === '.') {
+      if (secondNumber > 0 ) {
+        setSecondNumber(secondNumber + '')
+      } else {
+        setSecondNumber(e.target.innerText)
+      }
     } else {
-      setSecondNumber(e.target.innerText)
+      if (secondNumber > 0 || secondNumber === '.') {
+        setSecondNumber(secondNumber + e.target.innerText)
+      } else {
+        setSecondNumber(e.target.innerText)
+      }
     }
   }
 
@@ -28,13 +57,11 @@ function App() {
     setOperator(e.target.innerText)
   }
 
-  function handleClearFirstNumber(e) {
-    e.stopPropagation()
+  function handleClearFirstNumber() {
     setFirstNumber('0')
   }
 
-  function handleClearSecondNumber(e) {
-    e.stopPropagation()
+  function handleClearSecondNumber() {
     setSecondNumber('0')
   }
 
@@ -42,18 +69,15 @@ function App() {
     setTotal(eval(firstNumber + operator + secondNumber))
   }
 
-  function handleStore(e) {
-    e.stopPropagation()
+  function handleStore() {
     setStore(total)
   }
 
-  function recallFirst(e) {
-    e.stopPropagation()
+  function recallFirst() {
     setFirstNumber(store)
   }
 
-  function recallSecond(e) {
-    e.stopPropagation()
+  function recallSecond() {
     setSecondNumber(store)
   }
   
@@ -61,47 +85,49 @@ function App() {
     <div className="calculator">
       <div className="panel">
         <p>{firstNumber}</p>
-        <div className="numbers" onClick={handleClickFirstNumber}>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
+        <div className="numbers">
+          <button onClick={handleClickFirstNumber}>1</button>
+          <button onClick={handleClickFirstNumber}>2</button>
+          <button onClick={handleClickFirstNumber}>3</button>
+          <button onClick={handleClickFirstNumber}>4</button>
+          <button onClick={handleClickFirstNumber}>5</button>
+          <button onClick={handleClickFirstNumber}>6</button>
+          <button onClick={handleClickFirstNumber}>7</button>
+          <button onClick={handleClickFirstNumber}>8</button>
+          <button onClick={handleClickFirstNumber}>9</button>
+          <button onClick={handleClickFirstNumber}>0</button>
           <button onClick={handleClearFirstNumber}>Clear</button>
           <button onClick={recallFirst}>Recall</button>
+          <button onClick={handleClickFirstNumber}>.</button>
         </div>
       </div>
 
       <div className="panel">
         <p>{operator}</p>
-        <div className="numbers" onClick={handleClickOperator}>
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+        <div className="numbers">
+          <button onClick={handleClickOperator}>+</button>
+          <button onClick={handleClickOperator}>-</button>
+          <button onClick={handleClickOperator}>*</button>
+          <button onClick={handleClickOperator}>÷</button>
         </div>
       </div>
 
       <div className="panel">
         <p>{secondNumber}</p>
-        <div className="numbers" onClick={handleClickSecondNumber}>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
+        <div className="numbers">
+          <button onClick={handleClickSecondNumber}>1</button>
+          <button onClick={handleClickSecondNumber}>2</button>
+          <button onClick={handleClickSecondNumber}>3</button>
+          <button onClick={handleClickSecondNumber}>4</button>
+          <button onClick={handleClickSecondNumber}>5</button>
+          <button onClick={handleClickSecondNumber}>6</button>
+          <button onClick={handleClickSecondNumber}>7</button>
+          <button onClick={handleClickSecondNumber}>8</button>
+          <button onClick={handleClickSecondNumber}>9</button>
+          <button onClick={handleClickSecondNumber}>0</button>
           <button onClick={handleClearSecondNumber}>Clear</button>
           <button onClick={recallSecond}>Recall</button>
+          <button onClick={handleClickSecondNumber}>.</button>
         </div>
       </div>
       <div className="panel answer">
