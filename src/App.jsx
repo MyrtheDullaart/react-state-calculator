eaf21ed73bd5892dd336631ff9c264206add075f
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-23 16:13:48 +0200

    add appending numbers and store value functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,22 @@ function App() {
   const [secondNumber, setSecondNumber] = useState('0')
   const [operator, setOperator] = useState('+')
   const [total, setTotal] = useState('0')
+  const [store, setStore] = useState('0')
 
   function handleClickFirstNumber(e) {
-    setFirstNumber(e.target.innerText)
+    if (firstNumber > 0 ) {
+      setFirstNumber(firstNumber + e.target.innerText)
+    } else {
+      setFirstNumber(e.target.innerText)
+    }
   }
 
   function handleClickSecondNumber(e) {
-    setSecondNumber(e.target.innerText)
+    if (secondNumber > 0 ) {
+      setSecondNumber(secondNumber + e.target.innerText)
+    } else {
+      setSecondNumber(e.target.innerText)
+    }
   }
 
   function handleClickOperator(e) {
@@ -21,16 +30,31 @@ function App() {
 
   function handleClearFirstNumber(e) {
     e.stopPropagation()
-    setFirstNumber(0)
+    setFirstNumber('0')
   }
 
   function handleClearSecondNumber(e) {
     e.stopPropagation()
-    setSecondNumber(0)
+    setSecondNumber('0')
   }
 
   function handleCalculate() {
     setTotal(eval(firstNumber + operator + secondNumber))
+  }
+
+  function handleStore(e) {
+    e.stopPropagation()
+    setStore(total)
+  }
+
+  function recallFirst(e) {
+    e.stopPropagation()
+    setFirstNumber(store)
+  }
+
+  function recallSecond(e) {
+    e.stopPropagation()
+    setSecondNumber(store)
   }
   
   return (
@@ -49,6 +73,7 @@ function App() {
           <button>9</button>
           <button>0</button>
           <button onClick={handleClearFirstNumber}>Clear</button>
+          <button onClick={recallFirst}>Recall</button>
         </div>
       </div>
 
@@ -76,12 +101,14 @@ function App() {
           <button>9</button>
           <button>0</button>
           <button onClick={handleClearSecondNumber}>Clear</button>
+          <button onClick={recallSecond}>Recall</button>
         </div>
       </div>
       <div className="panel answer">
         <p>{total}</p>
         <div>
           <button onClick={handleCalculate}>=</button>
+          <button onClick={handleStore}>Store</button>
         </div>
       </div>
     </div>
